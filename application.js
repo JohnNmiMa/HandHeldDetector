036c89b45f90f9dc29c2826be3819f328d9588a8
John Marks <johnmarks@Johns-MBP-2.local>
2015-11-19 09:39:51 -0700

    Use actual pixel ratio

--- application.js
+++ application.js
@@ -24,25 +24,15 @@ $(document).ready(function() {
             visualViewportH = window.innerHeight,
             htmlW = document.documentElement.offsetWidth,
             htmlH = document.documentElement.offsetHeight,
-            mqPixelRatio1 = window.matchMedia( "(-webkit-max-device-pixel-ratio: 1)" ),
-            mqPixelRatio2 = window.matchMedia( "(-webkit-min-device-pixel-ratio: 1.1) and (-webkit-max-device-pixel-ratio: 4)" ),
-            mqPixelRatio5 = window.matchMedia( "(-webkit-min-device-pixel-ratio: 4.01)" ),
+            pixelRatioStr = window.devicePixelRatio ? window.devicePixelRatio : "window.devicePixelRation not supported",
             mqOrientation = window.matchMedia( "(orientation: portrait)" ),
             mqOrientationStr = mqOrientation.matches ? "Portrait" : "Landscape";
-
-        if (mqPixelRatio1.matches) {
-            mqPixelRatioStr = "PixelRatio = 1";
-        } else if (mqPixelRatio2.matches) {
-            mqPixelRatioStr = "PixelRatio = 1.1 - 4";
-        } else if (mqPixelRatio5.matches) {
-            mqPixelRatioStr = "PixelRatio > 4";
-        }
 
         var info = "Device info: screen width/height=" + scrW + "/" + scrH + "<br>" +
             "LayoutViewport width/height=" + layoutViewportW + "/" + layoutViewportH + "<br>" +
             "VisualViewport width/height=" + visualViewportW + "/" + visualViewportH + "<br>" +
             "<html> width/height=" + htmlW + "/" + htmlH + "<br>" +
-            mqPixelRatioStr + "<br>" + mqOrientationStr;
+            "Pixel ratio = " + pixelRatioStr + "<br>" + mqOrientationStr;
 
         return info;
     }
